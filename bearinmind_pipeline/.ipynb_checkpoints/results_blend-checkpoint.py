deaaa9f00f6cdfe5bb1f2cb5f516cbda08a17e71
bearindmind <bearindmind>
2019-11-11 17:04:24 +0000

    fixed minor issues in the results blend

--- bearinmind_pipeline/.ipynb_checkpoints/results_blend-checkpoint.py
+++ bearinmind_pipeline/.ipynb_checkpoints/results_blend-checkpoint.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class resultsBlender: 
 
-    def __init__(self):
+    def __init__(self, path_df1, path_df2):
         self.path_df1 = path_df1
         self.path_df2 = path_df2
     
@@ -20,12 +20,12 @@ class resultsBlender:
         df2 = pd.read_csv(self.path_df2)
         target1 = target + '_x'
         target2 = target + '_y'
-        df1 = df1.merge(df2, on = 'id')
+        df1 = df1.merge(df2, on = _id)
 
         dif = (df1[target1] - df1[target2]).abs().mean()
         print(f'the mean absolute difference between 2 submissions is: {dif}')
 
-        df1[target] = (proport*df1[target1] + (1-proport)*df1[target2])
+        df1[target] = (proport1*df1[target1] + (1-proport1)*df1[target2])
 
         df = df1[[_id, target]]
 
