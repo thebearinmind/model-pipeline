5e2521fced1c259fbc9860ef0293f092b1d38623
bearindmind <bearindmind>
2019-12-11 15:12:03 +0000

    incorporated hyper parameter tunning in the algorithm

--- bearinmind_pipeline/model_construction.py
+++ bearinmind_pipeline/model_construction.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import xgboost as xgb
 from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 
 class modelBuilder: 
@@ -82,10 +83,65 @@ class modelBuilder:
             plt.savefig(f'varImp/lgbm_importance.png')
             varimps = pd.DataFrame(feature_imp.sort_values(by="Value", ascending=False))
             varimps.to_csv(f'varImp/lgbm_importance.csv', index = False)
+    
+    def hyperparam_tuning(self, prep_data, search_type, param_grid, n_iter_hp, metric, n_folds, params = None):
+        """
+        Function to enable hyperparameters tunning. 
+        
+        Allowed type of searches: randomized, grid.
+        
+        """
+        X = prep_data['X']
+        Y = prep_data['Y']
+        X_test = prep_data['X_test']
+        
+        if self.problem_type == 'regression':
+            
+            if self.model_type == 'LGBM':
+                fit_model = lgb.LGBMRegressor(**params)
+            
+            elif self.model_type == 'CatBoost':
+                fit_model = cat.CatBoostRegressor(**params)                                                    
+            
+            elif self.model_type == 'XGBoost':
+                fit_model = xgb.XGBRegressor(**params)
 
+            else : 
+                print(f'This {self.model_type} is not yet supported!')
+
+        elif self.problem_type == 'classification':
+
+            if self.model_type == 'LGBM':
+                fit_model = lgb.LGBMClassifier(**params, verbose = verbose)
+
+            elif self.model_type == 'XGBoost':
+                fit_model = xgb.XGBClassifier(**params)
+
+            else: 
+                print(f'{self.model_type} for classification is not yet supported')
+
+        else: 
+            print(f'{self.problem_type} type of a problem solving is not yet supported')
+        
+        if search_type == 'randomized':
+        
+            gs = RandomizedSearchCV(estimator=fit_model, param_distributions=param_grid, n_iter=n_iter_hp, scoring=metric, cv=n_folds, refit=True, random_state=314, verbose=True)
+        
+        elif search_type == 'grid':
+            
+            gs = GridSearchCV(estimator=fit_model, param_distributions=param_grid, n_iter=n_iter_hp, scoring=metric, cv=n_folds, refit=True, random_state=314, verbose=True)
+            
+        else:
+            print(f'{self.search_type} type of a search is not yet supported')
+        
+        gs.fit(X, Y)
+
+        print('Best score reached: {} with params: {} '.format(gs.best_score_, gs.best_params_))
+
+        return(gs)
 
     def run_model(self, prep_data, n_folds, metric_func = roc_auc_score, get_probab = False, save_varimp = False, params = None, oversmp = False, verbose = 2, 
-                  cv_shuffle = True):
+                  cv_shuffle = True, hyperparam_tunning = False, search_type = None, param_grid = None, n_iter_hp = None, metric = None):
         """
         The model based on the problem type (regression or classification) as well as chosen algorithm (e.g. LightGBM) will be run across specified
         number of K folds. The metric will be displayed and saved in a list for each single fold. The final score with be represented as an average 
@@ -106,6 +162,11 @@ class modelBuilder:
         scores = []
         submit_pred = np.zeros((X_test.shape[0],1))
         
+        if hyperparam_tunning:
+            gs = self.hyperparam_tuning(prep_data = prep_data, search_type = search_type, param_grid = param_grid, n_iter_hp = n_iter_hp, metric = metric, n_folds = n_folds, params = params)
+        else:
+            pass
+        
         for i, (train_index, test_index) in enumerate(kf.split(X, Y)):
             # Create data for this fold
             Y_train, Y_valid = Y.iloc[train_index].copy(), Y.iloc[test_index].copy()
@@ -125,11 +186,12 @@ class modelBuilder:
 
                 if self.model_type == 'LGBM':
                     fit_model = lgb.LGBMRegressor(**params)
-                    fit_model.fit(X_train, Y_train)
-
+                    
                 elif self.model_type == 'CatBoost':
                     fit_model = cat.CatBoostRegressor(**params)                                                    
-                    fit_model.fit(X_train, Y_train,  verbose = False)
+                
+                elif self.model_type == 'XGBoost':
+                    fit_model = xgb.XGBRegressor(**params)
 
                 else : 
                      print(f'This {self.model_type} is not yet supported!')
@@ -138,11 +200,9 @@ class modelBuilder:
 
                 if self.model_type == 'LGBM':
                     fit_model = lgb.LGBMClassifier(**params, verbose = verbose)
-                    fit_model.fit(X_train, Y_train)
 
                 elif self.model_type == 'XGBoost':
                     fit_model = xgb.XGBClassifier(**params)
-                    fit_model.fit(X_train, Y_train)
 
                 else: 
                     print(f'{self.model_type} for classification is not yet supported')
@@ -150,6 +210,12 @@ class modelBuilder:
             else: 
                 print(f'{self.problem_type} type of a problem solving is not yet supported')
 
+            if hyperparam_tunning:
+                fit_model = gs.best_estimator_
+                fit_model.fit(X_train, Y_train)
+            else:
+                fit_model.fit(X_train, Y_train)
+            
             if get_probab:
                 pred = fit_model.predict_proba(X_valid)[:,1]
                 submit_pred += fit_model.predict_proba(X_test)[:,1]/n_folds
@@ -160,7 +226,8 @@ class modelBuilder:
                 full_fold_pred = np.reshape(full_fold_pred, (full_fold_pred.shape[0],1))
                 pred = fit_model.predict(X_valid)
                 submit_pred += full_fold_pred/n_folds
-
+                
+            
             # Save validation predictions for this fold
             score = metric_func(Y_valid, pred)
 
